cdac54c47e47046452132fdc8cc455628d71dec7
matheusburey <matheushburei.99@gmail.com>
2025-05-04 22:34:48 -0300

    style: set up custom fonts and color scheme in Tailwind config

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import "@/assets/global.css";
 
 import { Stack } from "expo-router";
-
+import * as SplashScreen from "expo-splash-screen";
 import {
 	useFonts,
 	Rubik_400Regular,
@@ -10,19 +10,27 @@ import {
 	Rubik_600SemiBold,
 	Rubik_700Bold,
 } from "@expo-google-fonts/rubik";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-	const [loaded] = useFonts({
+	const [loaded, error] = useFonts({
 		Rubik_400Regular,
 		Rubik_500Medium,
 		Rubik_600SemiBold,
 		Rubik_700Bold,
 	});
 
-	if (!loaded) {
+	useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+	if (!loaded && !error) {
 		return null;
 	}
-
 	return (
 		<>
 			<Stack
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,34 @@ module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        regular: "Rubik_400Regular",
+        medium: "Rubik_500Medium",
+        semiBold: "Rubik_600SemiBold",
+        bold: "Rubik_700Bold",
+      },
+      colors: {
+        gray: {
+          100: "#FCFDFE",
+          200: "#E1EBF4",
+          300: "#C4D0DB",
+          400: "#73808C",
+          500: "#45525F",
+          600: "#1A1F24",
+        },
+        green: {
+          soft: "#E9F3EF",
+          light: "#3B9B62",
+          base: "#257F49",
+          dark: "#052914",
+        },
+        red: {
+          light: "#FDEDED",
+          base: "#F94144",
+        },
+      },
+    },
   },
   plugins: [],
 }
